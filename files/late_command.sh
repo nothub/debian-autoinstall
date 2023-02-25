93efc4281c6e6ed69e28a546c388059fbe14b2d2
Florian Hübner <code@hub.lol>
2023-02-25 18:06:13 +0100

    log post install commands

--- files/late_command.sh
+++ files/late_command.sh
@@ -4,28 +4,41 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-url="https://nothub.github.io/debian-autoinstall/files"
+files_url="https://nothub.github.io/debian-autoinstall/files"
+ssh_keys_url="https://github.com/nothub.keys"
 
 # find user (from default userid)
 user=$(id -u -n -- "1000")
 user_home=$(getent passwd "${user}" | cut -d: -f6)
 
+log="${user_home}/install.log"
+
+echo "running post install commands" >"${log}"
+
 # expire user passwort (requires password to be defined on next login)
 passwd --delete "${user}"
 passwd --expire "${user}"
 
+echo "password expired for user ${user}" >"${log}"
+
 # download some config
-curl --location --output "/etc/ssh/sshd_config" "${url}/sshd_config"
-curl --location --output "${user_home}/.bashrc" "${url}/bashrc"
+curl --location --output "/etc/ssh/sshd_config" "${files_url}/sshd_config"
+curl --location --output "${user_home}/.bashrc" "${files_url}/bashrc"
+
+echo "downloaded configs" >"${log}"
 
 # authorize ssh login
 mkdir -p "${user_home}/.ssh"
 chmod 700 "${user_home}/.ssh"
-curl --location "https://github.com/nothub.keys" >"${user_home}/.ssh/authorized_keys"
+curl --location "${ssh_keys_url}" >"${user_home}/.ssh/authorized_keys"
 chmod 644 "${user_home}/.ssh/authorized_keys"
+
+echo "added ssh keys" >"${log}"
 
 # reset user homedir owner
 chown -R "$(stat --format "%U:%G" "${user_home}")" "${user_home}"
+
+echo "reset homedir perms for ${user}" >"${log}"
 
 # random art banner
 rand() {
@@ -39,5 +52,9 @@ buf+="$(for _ in {1..20}; do rand ".⋅∙⋆"; done)"
 buf+="$(for _ in {1..750}; do echo -n " "; done)"
 echo "${buf}" | grep -Eo '[^\n]{1}' | shuf | tr -d '\n' | grep -Eo '.{60}' >/etc/motd
 
+echo "random art banner" >"${log}"
+
 # install nix
 sh <(curl -L https://nixos.org/nix/install) --daemon
+
+echo "install nix package manager" >"${log}"

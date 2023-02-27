d51de53499925686a1deb9bd5f5bc5d820989641
Florian Hübner <code@hub.lol>
2023-02-28 00:49:24 +0100

    packages

--- iso.nix
+++ iso.nix
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/988cc958c57ce4350ec248d2d53087777f9e1949.tar.gz
-#! nix-shell -p cacert coreutils curl gnupg xorriso
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/7f5639fa3b68054ca0b062866dc62b22c3f11505.tar.gz
+#! nix-shell -p coreutils cacert curl gnupg xorriso
 #! nix-shell -i bash --pure
 #shellcheck shell=bash
 ./iso.sh

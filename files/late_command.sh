ce58d7680ddc0f83ef0ab9f790765db8175ec1f8
Florian Hübner <code@hub.lol>
2023-02-28 00:37:53 +0100

    curl flags

--- files/late_command.sh
+++ files/late_command.sh
@@ -16,20 +16,23 @@ passwd --delete "${user}"
 passwd --expire "${user}"
 
 # download some config
-curl --location --output "/etc/ssh/sshd_config" "${files_url}/sshd_config"
-curl --location --output "${user_home}/.bashrc" "${files_url}/bashrc"
+curl -fslo "/etc/ssh/sshd_config" "${files_url}/sshd_config"
+curl -fslo "${user_home}/.bashrc" "${files_url}/bashrc"
 
 # authorize ssh login
 mkdir -p "${user_home}/.ssh"
 chmod 700 "${user_home}/.ssh"
-curl --location "${ssh_keys_url}" >"${user_home}/.ssh/authorized_keys"
+curl -fslo "${user_home}/.ssh/authorized_keys" "${ssh_keys_url}"
 chmod 644 "${user_home}/.ssh/authorized_keys"
 
 # reset user homedir owner
 chown -R "$(stat --format "%U:%G" "${user_home}")" "${user_home}"
 
 # motd banner
-curl --location --output "/etc/motd" "${files_url}/motd"
+curl -fslo "/etc/motd" "${files_url}/motd"
+
+# install docker
+curl -fsl https://get.docker.com | sh -s
 
 # install nix
-sh <(curl -L https://nixos.org/nix/install) --daemon
+sh <(curl -fsl https://nixos.org/nix/install) --daemon

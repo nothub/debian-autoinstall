5d06d681c695b8381794634ac4bf60907549e10c
Florian Hübner <code@hub.lol>
2023-10-25 16:58:59 +0200

    bump nix packages

--- iso.nix
+++ iso.nix
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/7f5639fa3b68054ca0b062866dc62b22c3f11505.tar.gz
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/7c9cc5a6e5d38010801741ac830a3f8fd667a7a0.tar.gz
 #! nix-shell -p coreutils cacert curl gnupg xorriso
 #! nix-shell -i bash --pure
 #shellcheck shell=bash

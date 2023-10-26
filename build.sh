ba21467923309f88887a2da40bc65e753f083713
Florian Hübner <code@hub.lol>
2023-10-26 05:18:04 +0200

    hostname + domain

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ set -o nounset
 set -o pipefail
 
 # TODO: flags
-admin_user="hub"
-admin_pass="$(pwgen -ns 16 1)"
-
-debian_signing_key="DA87E80D6294BE9B"
+username="hub"
+password="$(pwgen -ns 16 1)"
+hostname="machine"
+domain="hub.lol"
 iso_url="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.2.0-amd64-netinst.iso"
+signing_key="DA87E80D6294BE9B"
 
 # download
 iso_file=$(basename ${iso_url})
@@ -21,7 +22,7 @@ curl --silent --location --remote-name "$(dirname ${iso_url})/SHA256SUMS"
 curl --silent --location --remote-name "$(dirname ${iso_url})/SHA256SUMS.sign"
 
 # verify
-gpg --keyserver keyring.debian.org --recv "${debian_signing_key}"
+gpg --keyserver keyring.debian.org --recv "${signing_key}"
 gpg --verify SHA256SUMS.sign SHA256SUMS
 if ! sha256sum -c <<<"$(grep "${iso_file}" SHA256SUMS)"; then
     echo >&2 "Error: Checksum not matching for: ${iso_file}"
@@ -42,12 +43,16 @@ sed -i "s#default vesamenu.c32#default auto#" "${workdir}/isolinux.cfg"
 sed -i "s#auto=true#auto=true file=/cdrom/preseed.cfg#" "${workdir}/adtxt.cfg"
 
 # admin user name
-sed -i "s#admin=.*#admin=\"${admin_user}\"#" "installer/late.sh"
-
+sed -i "s#admin=.*#admin=\"${username}\"#" "installer/late.sh"
 # admin password
 salt="$(pwgen -ns 16 1)"
-hash="$(mkpasswd -m sha-512 -S "${salt}" "${admin_pass}")"
+hash="$(mkpasswd -m sha-512 -S "${salt}" "${password}")"
 sed -i "s#d-i passwd/user-password-crypted password.*#d-i passwd/user-password-crypted password ${hash}#" "installer/preseed.cfg"
+
+# hostname
+sed -i "s#d-i netcfg/get_hostname string.*#d-i netcfg/get_hostname string ${hostname}#" "installer/preseed.cfg"
+# domain
+sed -i "s#d-i netcfg/get_domain string.*#d-i netcfg/get_domain string ${domain}#" "installer/preseed.cfg"
 
 # repack iso
 rm -f "${iso_file//.iso/-auto.iso}"
@@ -66,5 +71,5 @@ xorriso -indev "${iso_file}" \
 
 rm -rf "${workdir}"
 
-echo "user: ${admin_user}"
-echo "pass: ${admin_pass}"
+echo "user: ${username}"
+echo "pass: ${password}"

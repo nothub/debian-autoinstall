ab5aa09628e0dab4381a0d5eaf7f51d61e8efb98
Florian Hübner <code@hub.lol>
2023-11-25 22:53:09 +0100

    allow root ssh access (with certs only)

--- installer/late.sh
+++ installer/late.sh
@@ -30,11 +30,16 @@ cp -a "/cdrom/configs/issue"       "${prefix}/etc/issue"
 cp -a "/cdrom/configs/motd"        "${prefix}/etc/motd"
 cp -a "/cdrom/configs/sshd_config" "${prefix}/etc/ssh/sshd_config"
 
-# authorize ssh keys
+# authorize ssh keys for root user
+mkdir -p  "${prefix}/root/.ssh"
+chmod 700 "${prefix}/root/.ssh"
+cp -a "/cdrom/configs/authorized_keys" "${prefix}/root/.ssh/authorized_keys"
+chmod 640 "${prefix}/root/.ssh/authorized_keys"
+# authorize ssh keys for admin user
 mkdir -p  "${prefix}/home/${admin}/.ssh"
 chmod 700 "${prefix}/home/${admin}/.ssh"
 cp -a "/cdrom/configs/authorized_keys" "${prefix}/home/${admin}/.ssh/authorized_keys"
-chmod 644 "${prefix}/home/${admin}/.ssh/authorized_keys"
+chmod 640 "${prefix}/home/${admin}/.ssh/authorized_keys"
 
 # reset user homedir owner
 chown -R "1000:1000" "${prefix}/home/${admin}"
